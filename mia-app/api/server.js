31359236487f9e7e630d91b559e3ccc2dce00e0c
Takemyeye <mellovan2005@gmail.com>
2024-09-17 19:32:51 +0200

    \

--- mia-app/api/server.js
+++ mia-app/api/server.js
@@ -1,19 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const path = require('path');
+require('./auth/passport-setup');
+const connectDB = require('./db');
 const discordAuthRoutes = require('./auth/authCompany/discordAuth');
 const googleAuthRoutes = require('./auth/authCompany/googleAuth');
 const githubAuthRoutes = require('./auth/authCompany/gitHubAuth');
 const authRoutes = require('./auth/auth');
-const passport = require('passport');
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-require('./auth/passport-setup');
-require('dotenv').config();
 
-const connectDB = require('./db');
 connectDB();
 
 const app = express();
-const port = process.env.PORT || 3001;
 
 app.use(cors({
   origin: 'https://belance.netlify.app/',
@@ -24,9 +22,9 @@ app.use(cors({
 
 app.use(passport.initialize());
 
-app.use('/', googleAuthRoutes);
-app.use('/', githubAuthRoutes);
-app.use('/', discordAuthRoutes);
+app.use('/auth', googleAuthRoutes);
+app.use('/auth', githubAuthRoutes);
+app.use('/auth', discordAuthRoutes);
 
 app.use('/api', authRoutes);
 
@@ -34,6 +32,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
